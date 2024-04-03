0df254acbc9f6e6f6bf44b76d149aa3c10e7db54
Yuvraj <Yuvrajxt@gmail.com>
2024-04-03 17:52:05 +0530

    5:04:05

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -489,3 +489,27 @@ const random = checkInput('2020-05-05');
 
 console.log(year);
 console.log(random);
+
+
+// GENERICS
+
+ function generateStringArray(length:number,value:string):string[] {
+  let result :string[] =[];
+  result = Array(length).fill(value);
+  return result;
+ }
+
+ console.log(generateStringArray(6,'hello'));
+
+ function createArray<T>(length:number,value:T):Array<T>{
+  let result:T[] =[];
+  result = Array(length).fill(value);
+  return result;
+ }
+
+let arrayStrings = createArray<string>(10,'hello');
+let arrayNumbers = createArray<number>(15,100);
+
+console.log(arrayStrings);
+console.log(arrayNumbers);
+
